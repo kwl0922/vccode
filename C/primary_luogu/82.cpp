6bd112b74a968cc9e4cba0c85abf44ee5f9a90cc
lenovo <lenovo@LAPTOP-UT75V8MO>
2025-02-12 11:03:52 +0800

    1

--- C/primary_luogu/82.cpp
+++ C/primary_luogu/82.cpp
@@ -3,9 +3,12 @@
 #include<math.h>
 
 int main(){
-    int a[6],i,j;
+    int i;
+    double a[6],result;
     for(i=0;i<6;i+=2){
-        scanf("%d%d",a[i],a[i+1]);
+        scanf("%lf%lf",&a[i],&a[i+1]);
     }
+    result=sqrt((a[0]-a[2])*(a[0]-a[2])+(a[1]-a[3])*(a[1]-a[3]))+sqrt((a[0]-a[4])*(a[0]-a[4])+(a[1]-a[5])*(a[1]-a[5]))+sqrt((a[2]-a[4])*(a[2]-a[4])+(a[3]-a[5])*(a[3]-a[5]));
+    printf("%.2lf",result);
     return 0;
 }
