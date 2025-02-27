c9468f6405ada2dee9e249fb36d260c4142425c0
lenovo <lenovo@LAPTOP-UT75V8MO>
2025-02-27 22:09:45 +0800

    25/2/27

--- Python/1.py
+++ Python/1.py
@@ -1,1 +1,17 @@
-print('Hello world!')
+#单行注释符
+"""
+多行注释符
+"""
+print("Hello world!")
+a = 10
+b = 20
+c = '立白'
+print(a + b , c)
+name = input('请输入你的名字:')
+print(name)
+name2: str = 'name'
+age: int = 25
+f:float = 1.22
+print(type(name2),type(age),type(f))
+#取模
+print(100 // 6, 100 % 6)#16*6+4=100，16是模，4是余数
